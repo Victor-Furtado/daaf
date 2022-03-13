923825e36737beb3e7e413d6063ec318aef69fb3
Victor Furtado <stolen@pop-os.localdomain>
2022-03-13 16:47:21 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+);
+
+export default IndexPage;
